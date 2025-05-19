e34694dc5758758b637602dc18b9ebaddd2eb036
Rochyiat <rochyiatramdani@gmail.com>
2025-05-19 11:15:42 +0700

    tugas dari pak dosen tegar

--- fetchApi/fetch.js
+++ fetchApi/fetch.js
@@ -6,8 +6,14 @@ fetch("http://localhost:3000/api/users", {
 })
   .then((response) => response.json())
   .then((data) => {
+    const list = document.getElementById("data");
+    
     data.data.forEach((user) => {
-        console.log(`Name: ${user.name}, Email: ${user.email}`);
+        // console.log(`Name: ${user.name}, Email: ${user.email}`);
+
+        const li = document.createElement("li");
+        li.textContent = `Name: ${user.name}, Email: ${user.email}`;
+        list.appendChild(li);
     });
   })
   .catch((error) => {
